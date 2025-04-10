1b69689e5fdcde4ac01786bee03bcbc93e6e42ea
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2025-04-10 20:35:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-
+#include <string>
 #include "include/core/scanner/signature_scanner.h"
 
-int main(){
-    // Example usage of the SignatureScanner class
-    scanner::SignatureScanner scanner;
-    std::string filePath = "example.txt"; // Replace with your file path
-    scanner::Signature signature = scanner.getFileSignatureSHA256(filePath);
-    std::cout << signature << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " -getFileSignature256 <filePath>" << std::endl;
+        return 1;
+    }
 
-    getchar();
+    std::string command = argv[1];
+    std::string filePath = argv[2];
+
+    if (command == "-getFileSignature256") {
+        scanner::SignatureScanner scanner;
+        scanner::Signature signature = scanner.getFileSignatureSHA256(filePath);
+        std::cout << "SHA256 Signature: " << signature << std::endl;
+    } else {
+        std::cerr << "Unknown command: " << command << std::endl;
+        return 1;
+    }
+
     return 0;
 }
